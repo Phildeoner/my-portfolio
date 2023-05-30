bd4465978eb777cd60d306e0db06e060ee2f6352
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-05-30 10:13:07 +0100

    Create onscroll function in script.js (#12)

--- my_portfolio/resources/js/script.js
+++ my_portfolio/resources/js/script.js
@@ -22,3 +22,22 @@ function toggleMenu() {
 }
 
 menuBtn.addEventListener('click', toggleMenu);
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 150;
+    const height = sec.offsetHeight;
+    const id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove('active');
+        document.querySelector(`header nav a[href*=${id}]`).classList.add('active');
+      });
+    }
+  });
+};
